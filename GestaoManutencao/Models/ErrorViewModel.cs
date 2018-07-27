ab67d3c984ed76c1822db0cbfa94ce90f10df22b
Ezequiel Daniel <ezequielsd@gmail.com>
2018-07-27 16:49:41 -0300

    Versionando o projeto.

---
+++ GestaoManutencao/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestaoManutencao.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
